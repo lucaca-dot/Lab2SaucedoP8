f7ccfda158a3ba7d961a619d63ddae3d6c777df0
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2025-05-12 15:27:38 -0700

    I started  lab 3
    I started lab 3

---
+++ P8SaucedoLab2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Rigidbody playerRb;
+    private float zbound = 6
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        if(transform.position.z < -zbound)
+        {
+
+        }
+    }
+}
